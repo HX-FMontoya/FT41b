0a5ad49d767602eba287c450b7f1cdae76c33324
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-08-07 11:27:58 -0500

    hw bundler done

--- FT-M2-master/05-Bundlers/homework/browser/app.js
+++ FT-M2-master/05-Bundlers/homework/browser/app.js
@@ -1,25 +1,49 @@
-(function () {
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+// (function () {
+// IIFE
+/* var whiteboard = window.whiteboard;
+  var socket = window.io(window.location.origin); */
 
-  socket.on("connect", function () {
-    console.log("Connected!");
-  });
+// CommonJS
+/* const whiteboard = require("./whiteboard");
+const io = require("socket.io-client");
+const socket = io(window.location.origin); */
 
-  socket.on("load", function (strokes) {
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
-  });
+// ES6
+import whiteboard from "./whiteboard";
+import io from "socket.io-client";
+const socket = io(window.location.origin);
 
-  socket.on("draw", function (start, end, color) {
+// app -> raiz -> el que recibe todo
+// whiteboard -> recibe/requiere y da/exporta
+// event-emitter -> da/exporta
+
+socket.on("connect", function () {
+  console.log("Connected!");
+});
+
+socket.on("load", function (strokes) {
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
     whiteboard.draw(start, end, color, false);
   });
+});
 
-  whiteboard.on("draw", function (start, end, color) {
-    socket.emit("draw", start, end, color);
-  });
-})();
+socket.on("draw", function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
+
+whiteboard.on("draw", function (start, end, color) {
+  socket.emit("draw", start, end, color);
+});
+
+// })();
+
+// Tanto CommonJS como con ES6 no es necesario exportar nada porque app es el nodo raiz, el entry point
+
+/// Archivo que importa/requiere
+
+/// Archivos que requieren y al mismo tiempo exportan -> whiteboard
+
+/// Archivos que solo exportan -> event-emmiter
